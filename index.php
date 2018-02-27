b35d71c7eb9f7933893e91c8586c0598a8a6f5ed
Renan Pigato <renan@renanpigato.inf.br>
2018-02-27 00:28:46 -0300

    Ajutes nas telas de geração e index

--- index.php
+++ index.php
@@ -1,16 +1,3 @@
-<?php
-require_once 'conf.php';
-
-use Classes\Gerador;
-
-echo "Gerando nomes...";
-
-$gerador = new Gerador;
-$qtde = $gerador->gerarNomes();
-
-echo "{$qtde} nomes gerados.";
-
-?>
 <!DOCTYPE html>
 <html>
 <head>
